7f21aeae069348cad31a585bd15dd2c2628517c7
Ariel Ben Horesh <ariel_bh@yahoo.com>
2012-09-12 01:09:49 +0300

    Implemented 1st scenario of simple command.
    For both MVVMLight and RxUI. SIde by side
    
    
    Former-commit-id: 9e1030b07865daf48c95f75e8c34601b046470b1

---
+++ ReactiveUI.Samples.Commands/MVVMLight/MainViewModel.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+
+namespace ReactiveUI.Samples.Commands.MVVMLight
+{
+    class MainViewModel : ViewModelBase
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value != _name)
+                {
+                    _name = value;
+                    RaisePropertyChanged(() => Name);
+
+                    DisplayCommand.RaiseCanExecuteChanged();
+                }
+            }
+        }
+
+        private RelayCommand _displayCommand;
+
+        public RelayCommand DisplayCommand
+        {
+            get
+            {
+                return _displayCommand ?? (_displayCommand = new RelayCommand(
+                                                     () => MessageBox.Show("You clicked on DisplayCommand: Name is " +
+                                                                           Name),
+                                                     () => !string.IsNullOrEmpty(Name)));
+            }
+        }
+
+
+
+    }
+}

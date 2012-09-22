e9dd6f352e7914365635c0b7b467b5c190d74569
Ariel Ben Horesh <ariel_bh@yahoo.com>
2012-09-22 14:07:56 +0300

    Basics Scenario commit
    Former-commit-id: 8364d5c3938ede438b4ad3f1b12066c6804c4a7d

---
+++ ReactiveUI.Samples.Basics/ViewModels/MainViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ReactiveUI.Samples.Basics.ViewModels
+{
+    public class MainViewModel : ReactiveObject
+    {
+
+        public MainViewModel()
+        {
+            Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+                    if (Progress == 100)
+                    {
+                        Application.Current.Dispatcher.BeginInvoke(new Action(() =>
+                        {
+                            Progress = 0;
+                        }));
+
+                    }
+                    Application.Current.Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                        Progress++;
+                    }));
+                    
+                    Thread.Sleep(400);
+                }
+
+            });
+        }
+
+        private int _Progress;
+
+        public int Progress
+        {
+            get { return _Progress; }
+            set { this.RaiseAndSetIfChanged(x => x.Progress, value); }
+        }
+         
+    }
+}

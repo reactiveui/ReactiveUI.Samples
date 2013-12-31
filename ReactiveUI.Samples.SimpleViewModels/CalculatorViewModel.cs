9d90e4f8bf2918acdf03e4c76fbd060d7279ead9
Gordon Watts <gordon@gordonandpaula.net>
2013-12-31 00:51:32 -0500

    The no-time-needed test example.
    Former-commit-id: 528b3c577d23eb718c5742df59c999ee1f8e02b4

---
+++ ReactiveUI.Samples.SimpleViewModels/CalculatorViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace ReactiveUI.Samples.SimpleViewModels
+{
+    /// <summary>
+    /// A view model with some straight forward calculations. There are no
+    /// async operations involve, nor are there any delays or other time related
+    /// calls.
+    /// Operation: The user enters text into the input text field.
+    /// If the text doesn't contain numbers, then an error message is shown in the ErrorText field
+    /// Otherwise the number x2 is shown in the ResultText field.
+    /// The ErrorText and ResultText fields should be empty when nothing is in InputText.
+    /// </summary>
+    public class CalculatorViewModel : ReactiveObject
+    {
+        string _InputText;
+        public string InputText
+        {
+            get { return _InputText; }
+            set { this.RaiseAndSetIfChanged(ref _InputText, value); }
+        }
+
+        public string ErrorText { get; private set; }
+        private ObservableAsPropertyHelper<string> _ErrorTextOAPH;
+
+        public string ResultText { get; private set; }
+        private ObservableAsPropertyHelper<string> _ResultTextOAPH;
+
+        public CalculatorViewModel()
+        {
+            var haveInput = this.WhenAny(x => x.InputText, x => x.Value)
+                .Where(x => !string.IsNullOrEmpty(x));
+
+            // Convert into a stream of parsed integers, or null if we fail.
+            var parsedIntegers = haveInput
+                .Select(x =>
+                {
+                    int val;
+                    return int.TryParse(x, out val) ? (int?)val : (int?)null;
+                });
+
+            // Now, the error text
+            parsedIntegers
+                .Select(x => x.HasValue ? "" : "Error")
+                .Subscribe(x => { ErrorText = x; });
+
+            // And the result, which is *2 of the input.
+            parsedIntegers
+                .Select(x => x.HasValue ? (x.Value * 2).ToString() : "")
+                .Subscribe(x => { ResultText = x; });
+            // Woudd like be using the following line instead of above, but it doesn't work:
+            // ToProperty(this, x => x.ResultText, out _ResultTextOAPH);
+        }
+    }
+}
---
+++ ReactiveUI.Samples.SimpleViewModelsUnitTests/CalculatorViewModelTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReactiveUI.Samples.SimpleViewModels;
+
+namespace ReactiveUI.Samples.SimpleViewModelsUnitTests
+{
+    /// <summary>
+    /// A few sample unit tests. Note how simple they are: since the concept of time
+    /// is not involved we do not have to model it in our unit tests. We test these almost
+    /// as if they were a non ReactiveUI object.
+    /// </summary>
+    [TestClass]
+    public class CalculatorViewModelTest
+    {
+        [TestMethod]
+        public void TestTypingStringGetsError()
+        {
+            var fixture = new CalculatorViewModel();
+            fixture.InputText = "hi";
+            Assert.AreEqual("Error", fixture.ErrorText, "ErrorText");
+            Assert.AreEqual("", fixture.ResultText, "ResultText");
+        }
+
+        [TestMethod]
+        public void TestTypingInteger()
+        {
+            var fixture = new CalculatorViewModel();
+            fixture.InputText = "50";
+            Assert.AreEqual("", fixture.ErrorText, "ErrorText");
+            Assert.AreEqual("100", fixture.ResultText, "ResultText");
+        }
+    }
+}

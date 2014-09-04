13ced26d520513c9cf15329653454140d8ced5a0
Oren Novotny <oren@novotny.org>
2014-09-04 11:04:11 -0400

    Retarget to ReactiveWatchList
    Former-commit-id: bba79b3e3a2e3ab19b22f9e2a3d00e0796244311

---
+++ ReactiveWatchList/ReactiveWatchList/App.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace ReactiveWatchList
+{
+    public class App
+    {
+        public static Page GetMainPage()
+        {
+            return new ContentPage
+            {
+                Content = new Label
+                {
+                    Text = "Hello, Forms !",
+                    VerticalOptions = LayoutOptions.CenterAndExpand,
+                    HorizontalOptions = LayoutOptions.CenterAndExpand,
+                },
+            };
+        }
+    }
+}

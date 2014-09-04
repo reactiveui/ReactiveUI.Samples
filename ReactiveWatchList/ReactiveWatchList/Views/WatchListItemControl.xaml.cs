f16a787c4bf5b6d492e83c88f76a1591a3fedca1
Oren Novotny <oren@novotny.org>
2014-09-04 13:10:29 -0400

    Add view for watch list item
    Former-commit-id: c30d8687a55ab9f1c5dcbe07a644fd06b3f183f0

---
+++ ReactiveWatchList/ReactiveWatchList/Views/WatchListItemControl.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactiveWatchList.Views
+{
+    public partial class WatchListItemControl
+    {
+        public WatchListItemControl()
+        {
+            InitializeComponent();
+        }
+    }
+}

a98dac64466fcf4e11c4f896e680dcb7e0b49f8c
denisivan0v <ivanoff.denis@gmail.com>
2015-02-02 23:18:32 +0600

    PR preparing

---
+++ ReactiveUI.Samples.UniversalAppDemo/ReactiveUI.Samples.UniversalAppDemo.Shared/ViewModels/HubViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using ReactiveUI.Samples.UniversalAppDemo.Data;
+
+namespace ReactiveUI.Samples.UniversalAppDemo.ViewModels
+{
+    public class HubViewModel : ReactiveObject, IRoutableViewModel
+    {
+        private SampleDataGroup _groupToNavigate;
+        private SampleDataItem _itemToNavigate;
+
+        public HubViewModel(IScreen screen)
+        {
+            HostScreen = screen;
+            NavigateToSectionCommand = ReactiveCommand.Create();
+            NavigateToItemCommand = ReactiveCommand.Create();
+            GoBackCommand = ReactiveCommand.Create();
+
+            this.WhenAnyValue(x => x.GroupToNavigate)
+                .Where(x => x != null)
+                .Subscribe(x => HostScreen.Router.Navigate.Execute(new SectionViewModel(HostScreen, x)));
+
+            this.WhenAnyValue(x => x.ItemToNavigate)
+                .Where(x => x != null)
+                .Subscribe(x => HostScreen.Router.Navigate.Execute(new ItemViewModel(HostScreen, x)));
+
+            this.WhenNavigatedTo(() =>
+                {
+                    SetGroups();
+                    return Disposable.Empty;
+                });
+        }
+
+        public ReactiveCommand<object> NavigateToSectionCommand { get; private set; }
+        public ReactiveCommand<object> NavigateToItemCommand { get; private set; }
+        public ReactiveCommand<object> GoBackCommand { get; private set; }
+
+        public SampleDataGroup[] Groups { get; private set; }
+
+        public SampleDataGroup GroupToNavigate 
+        {
+            get { return _groupToNavigate; }
+            set { this.RaiseAndSetIfChanged(ref _groupToNavigate, value); }
+        }
+
+        public SampleDataItem ItemToNavigate
+        {
+            get { return _itemToNavigate; }
+            set { this.RaiseAndSetIfChanged(ref _itemToNavigate, value); }
+        }
+        
+        public IScreen HostScreen { get; private set; }
+
+        public string UrlPathSegment
+        {
+            get { return "hubpage"; }
+        }
+
+        private async void SetGroups()
+        {
+            Groups = await SampleDataSource.GetGroupsAsync();
+        }
+    }
+}
---
+++ ReactiveUI.Samples.UniversalAppDemo/ReactiveUI.Samples.UniversalAppDemo.Windows/SectionPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using ReactiveUI;
+
+using Windows.UI.Xaml.Controls;
+using ReactiveUI.Samples.UniversalAppDemo.Data;
+using ReactiveUI.Samples.UniversalAppDemo.ViewModels;
+
+namespace ReactiveUI.Samples.UniversalAppDemo
+{
+    public sealed partial class SectionPage : IViewFor<SectionViewModel>
+    {
+        public SectionPage()
+        {
+            InitializeComponent();
+
+            this.WhenAnyValue(x => x.ViewModel)
+                .Subscribe(x => DataContext = x);
+
+            this.WhenAnyObservable(x => x.ViewModel.NavigateToItemCommand)
+                .Subscribe(x =>
+                {
+                    var eventPattern = (ItemClickEventArgs)x;
+
+                    var sampleDataItem = (SampleDataItem)eventPattern.ClickedItem;
+                    ViewModel.HostScreen.Router.Navigate.Execute(new ItemViewModel(ViewModel.HostScreen, sampleDataItem));
+                });
+
+            this.WhenAnyObservable(x => x.ViewModel.GoBackCommand)
+                .Subscribe(x => ViewModel.HostScreen.Router.NavigateBack.Execute(null));
+
+        }
+        
+        object IViewFor.ViewModel
+        {
+            get { return ViewModel; }
+            set { ViewModel = (SectionViewModel)value; }
+        }
+
+        public SectionViewModel ViewModel { get; set; }
+    }
+}

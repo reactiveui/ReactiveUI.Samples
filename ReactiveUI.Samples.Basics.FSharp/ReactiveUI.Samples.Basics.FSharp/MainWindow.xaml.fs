521b83a42ebc0e27921837c18c37a6c181c55a03
TeaDrivenDev <TeaDrivenDev@gmail.com>
2015-12-26 18:21:07 +0100

    Added ReactiveUI.Samples.Basics.FSharp

---
+++ ReactiveUI.Samples.Basics.FSharp/ReactiveUI.Samples.Basics.FSharp/MainWindow.xaml.fs
@@ -0,0 +1,12 @@
+﻿namespace ViewModels
+
+open System
+open System.Windows
+open FSharp.ViewModule
+open FSharp.ViewModule.Validation
+open FsXaml
+
+type MainView = XAML<"MainWindow.xaml", true>
+
+type MainViewModel() as self = 
+    inherit ViewModelBase()    

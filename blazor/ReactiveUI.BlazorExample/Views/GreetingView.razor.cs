8fce3ab555aed95503fd5774dc6955af17ceeefa
Richard Bryant <rich.bryant@outlook.com>
2019-12-09 16:59:30 +0000

    Added BlazorExample

---
+++ blazor/ReactiveUI.BlazorExample/Views/GreetingView.razor.cs
@@ -0,0 +1,22 @@
+﻿using ReactiveUI.Blazor;
+using ReactiveUI.BlazorExample.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactiveUI.BlazorExample.Views
+{
+    public partial class GreetingView : ReactiveComponentBase<GreetingViewModel>
+    {
+        public GreetingView()
+        {
+            ViewModel = new GreetingViewModel();
+        }
+
+        public void Clear()
+        {
+            ViewModel.Clear.Execute().Subscribe();
+        }
+    }
+}

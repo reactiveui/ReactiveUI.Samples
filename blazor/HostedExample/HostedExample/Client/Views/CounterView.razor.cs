0669d87af75df745a3d332f4c2fe62530c6ea12f
Rich Bryant <rich.bryant@live.co.uk>
2020-01-12 00:35:39 +0000

    Moved files around, added Hosted example.

---
+++ blazor/HostedExample/HostedExample/Client/Views/CounterView.razor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ReactiveUI;
+using HostedExample.Client.ViewModels;
+
+
+
+namespace HostedExample.Client.Views
+{
+    public partial class CounterView : IViewFor<CounterViewModel>
+    {
+        public CounterView()
+        {
+            ViewModel = new CounterViewModel();
+        }
+
+        public CounterViewModel ViewModel { get; set; }
+        object IViewFor.ViewModel 
+        {
+            get => ViewModel;
+            set => ViewModel = (CounterViewModel)value;
+        }
+
+        private void IncrementCount()
+        {
+            ViewModel.Increment.Execute().Subscribe();
+        }
+    }
+}
---
+++ blazor/HostedExample/HostedExample/Client/Views/FetchDataView.razor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using HostedExample.Client.ViewModels;
+using Microsoft.AspNetCore.Components;
+using ReactiveUI;
+
+namespace HostedExample.Client.Views
+{
+    public partial class FetchDataView : IViewFor<FetchDataViewModel>
+    {
+        [Inject]
+        public FetchDataViewModel ViewModel { get; set; }
+
+
+        object IViewFor.ViewModel
+        {
+            get => ViewModel;
+            set => ViewModel = (FetchDataViewModel)value;
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await ViewModel.LoadForecasts.Execute().SubscribeOn(RxApp.MainThreadScheduler);
+        }
+    }
+}

9b087324e3cb89870e9663a5a101797465df7f11
Rich Bryant <rich.bryant@live.co.uk>
2020-02-02 12:45:54 +0000

    Serverside example updated.

--- blazor/ServerSideExample/ServerSideExample/Views/CounterView.razor.cs
+++ blazor/ServerSideExample/ServerSideExample/Views/CounterView.razor.cs
@@ -1,22 +1,14 @@
 ﻿using System.Reactive.Threading.Tasks;
 using System.Threading.Tasks;
-using ReactiveUI;
 using ServerSideExample.ViewModels;
 
 namespace ServerSideExample.Views
 {
-    public partial class CounterView : IViewFor<CounterViewModel>
+    public partial class CounterView
     {
         public CounterView()
         {
             ViewModel = new CounterViewModel();
-        }
-
-        public CounterViewModel ViewModel { get; set; }
-        object IViewFor.ViewModel 
-        {
-            get => ViewModel;
-            set => ViewModel = (CounterViewModel)value;
         }
 
         private async Task IncrementCount()

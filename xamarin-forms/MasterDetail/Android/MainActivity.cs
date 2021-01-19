8fe9c488e8ae57c042d9232397a8a64250c0a802
parth chopra <parthchopra18@gmail.com>
2021-01-19 18:39:00 -0500

    Update Master Detail Sample (#289)
    Co-authored-by: Parth Chopra <pchopra@yprime.com>

--- xamarin-forms/MasterDetail/Android/MainActivity.cs
+++ xamarin-forms/MasterDetail/Android/MainActivity.cs
@@ -6,11 +6,12 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Xamarin.Forms.Platform.Android;
 
 namespace MasterDetail.Droid
 {
     [Activity(Label = "MasterDetailDemo", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    public class MainActivity : FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {

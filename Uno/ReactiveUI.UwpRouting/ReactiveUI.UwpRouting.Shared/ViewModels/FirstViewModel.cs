172cf5c948734eb49c4c4235526c8536a6c72681
Chris Pulman <chris.pulman@yahoo.com>
2021-12-02 20:37:08 +0000

    Update examples to use Net6 where possible
    3 projects failing to build at this check-in

--- Uno/ReactiveUI.UwpRouting/ReactiveUI.UwpRouting.Shared/ViewModels/FirstViewModel.cs
+++ Uno/ReactiveUI.UwpRouting/ReactiveUI.UwpRouting.Shared/ViewModels/FirstViewModel.cs
@@ -1,17 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace ReactiveUI.UwpRouting.ViewModels
 {
     public class FirstViewModel : ReactiveObject, IRoutableViewModel
     {
+        public FirstViewModel(IScreen screen) => HostScreen = screen;
+
         // Reference to IScreen that owns the routable view model.
         public IScreen HostScreen { get; }
 
         // Unique identifier for the routable view model.
         public string UrlPathSegment { get; } = Guid.NewGuid().ToString().Substring(0, 5);
-
-        public FirstViewModel(IScreen screen) => HostScreen = screen;
     }
 }

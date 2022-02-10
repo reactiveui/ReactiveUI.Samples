b7674c6da3509072220d14e677bb637734aee654
Mauro Bertoli <info@stellarsolutions.it>
2022-02-10 11:38:47 +0100

    upgrade to .net core 6 and latest c# functionality, aligned project namespace to folder name, make VS Designer available for UserControl (#516)

--- winforms/demo/ReactiveDemo/MainWindow.cs
+++ winforms/demo/ReactiveDemo/MainWindow.cs
@@ -1,59 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Reactive.Disposables;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Reactive.Disposables;
 using System.Windows.Forms;
 using ReactiveUI;
-using ReactiveUI.Winforms;
 
-namespace ReactiveDemo
+namespace ReactiveUIDemo;
+
+public partial class MainWindow : Form, IViewFor<AppViewModel>
 {
-    public partial class MainWindow : Form, IViewFor<AppViewModel>
+    public MainWindow()
     {
-        public MainWindow()
+        InitializeComponent();
+
+        ViewModel = new AppViewModel();
+
+        // We create our bindings here. These are the code behind bindings which allow
+        // type safety. The bindings will only become active when the Window is being shown.
+        // We register our subscription in our disposableRegistration, this will cause
+        // the binding subscription to become inactive when the Window is closed.
+        // The disposableRegistration is a CompositeDisposable which is a container of
+        // other Disposables. We use the DisposeWith() extension method which simply adds
+        // the subscription disposable to the CompositeDisposable.
+        this.WhenActivated(disposableRegistration =>
         {
-            InitializeComponent();
-
-            ViewModel = new AppViewModel();
-
-            // We create our bindings here. These are the code behind bindings which allow
-            // type safety. The bindings will only become active when the Window is being shown.
-            // We register our subscription in our disposableRegistration, this will cause
-            // the binding subscription to become inactive when the Window is closed.
-            // The disposableRegistration is a CompositeDisposable which is a container of
-            // other Disposables. We use the DisposeWith() extension method which simply adds
-            // the subscription disposable to the CompositeDisposable.
-            this.WhenActivated(disposableRegistration =>
-            {
-                this.OneWayBind(ViewModel,
+            this.OneWayBind(ViewModel,
                     viewModel => viewModel.IsAvailable,
                     view => view.searchResultsListBox.Visible)
-                    .DisposeWith(disposableRegistration);
+                .DisposeWith(disposableRegistration);
 
-                this.OneWayBind(ViewModel,
+            this.OneWayBind(ViewModel,
                     viewModel => viewModel.SearchResults,
                     view => view.searchResultsListBox.Controls,
                     vmToViewConverterOverride: new ListBoxItemConverter())
-                    .DisposeWith(disposableRegistration);
+                .DisposeWith(disposableRegistration);
 
-                this.Bind(ViewModel,
+            this.Bind(ViewModel,
                     viewModel => viewModel.SearchTerm,
                     view => view.searchTextBox.Text)
-                    .DisposeWith(disposableRegistration);
-            });
-        }
-
-        object IViewFor.ViewModel
-        {
-            get => ViewModel;
-            set => ViewModel = (AppViewModel)value;
-        }
-
-        public AppViewModel ViewModel { get; set; }
+                .DisposeWith(disposableRegistration);
+        });
     }
+
+    object IViewFor.ViewModel
+    {
+        get => ViewModel;
+        set => ViewModel = (AppViewModel)value;
+    }
+
+    public AppViewModel ViewModel { get; set; }
 }
--- winforms/demo/ReactiveDemo/NugetDetailsView.cs
+++ winforms/demo/ReactiveDemo/NugetDetailsView.cs
@@ -1,39 +1,55 @@
-﻿using System.Reactive.Disposables;
+﻿using System.ComponentModel;
+using System.Reactive.Disposables;
+using System.Windows.Forms;
 using ReactiveUI;
-using ReactiveUI.Winforms;
 
-namespace ReactiveDemo
+namespace ReactiveUIDemo;
+
+// Visual Studio Designer does not support generic UserControls, so we cannot inherit from the generic `ReactiveUserControl<TViewModel>` and benefit from the RAD experience.
+// if you are not interested in the Designer RAD experience, you can inherit from `ReactiveUserControl<NugetDetailsViewModel>` and remove the `IViewFor` interface members (ViewModel and IViewFor.ViewModel)
+// You can take a look to the `ReactiveUserControl` implementation for more details.
+public partial class NugetDetailsView : UserControl, IViewFor<NugetDetailsViewModel> // or NugetDetailsView : ReactiveUserControl<TViewModel> if you don't need to use the designer
 {
-    public partial class NugetDetailsView : ReactiveUserControl<NugetDetailsViewModel>
+    [Category("ReactiveUI")]
+    [Description("The ViewModel.")]
+    [Bindable(true)]
+    [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+    public NugetDetailsViewModel? ViewModel { get; set; }
+
+    /// <inheritdoc/>
+    object? IViewFor.ViewModel
     {
-        public NugetDetailsView()
+        get => ViewModel;
+        set => ViewModel = (NugetDetailsViewModel?)value;
+    }
+
+    public NugetDetailsView()
+    {
+        InitializeComponent();
+
+        this.WhenActivated(disposableRegistration =>
         {
-            InitializeComponent();
+            // Our 4th parameter we convert from Url into a BitmapImage. 
+            // This is an easy way of doing value conversion using ReactiveUI binding.
+            this.OneWayBind(ViewModel,
+                    viewModel => viewModel.IconUrl,
+                    view => view.iconImage.ImageLocation)
+                .DisposeWith(disposableRegistration);
 
-            this.WhenActivated(disposableRegistration =>
-            {
-                // Our 4th parameter we convert from Url into a BitmapImage. 
-                // This is an easy way of doing value conversion using ReactiveUI binding.
-                this.OneWayBind(ViewModel,
-                        viewModel => viewModel.IconUrl,
-                        view => view.iconImage.ImageLocation)
-                    .DisposeWith(disposableRegistration);
+            this.OneWayBind(ViewModel,
+                    viewModel => viewModel.Title,
+                    view => view.titleRun.Text)
+                .DisposeWith(disposableRegistration);
 
-                this.OneWayBind(ViewModel,
-                        viewModel => viewModel.Title,
-                        view => view.titleRun.Text)
-                    .DisposeWith(disposableRegistration);
+            this.OneWayBind(ViewModel,
+                    viewModel => viewModel.Description,
+                    view => view.descriptionRun.Text)
+                .DisposeWith(disposableRegistration);
 
-                this.OneWayBind(ViewModel,
-                        viewModel => viewModel.Description,
-                        view => view.descriptionRun.Text)
-                    .DisposeWith(disposableRegistration);
-
-                this.BindCommand(ViewModel,
-                        viewModel => viewModel.OpenPage,
-                        view => view.titleRun)
-                    .DisposeWith(disposableRegistration);
-            });
-        }
+            this.BindCommand(ViewModel,
+                    viewModel => viewModel.OpenPage,
+                    view => view.titleRun)
+                .DisposeWith(disposableRegistration);
+        });
     }
 }

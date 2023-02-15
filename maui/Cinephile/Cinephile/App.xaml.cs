2f7a58653ec3e0a125f15ea70e12ddd33d756b95
Nathaniel Bond <nabond251@gmail.com>
2023-02-14 20:29:36 -0500

    Add MAUI Cinephile project

---
+++ maui/Cinephile/Cinephile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Cinephile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ maui/Cinephile/Cinephile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Cinephile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}

26538ce3ef9159256a87ffe4c419dec70f433c25
Nathaniel Bond <nabond251@gmail.com>
2023-02-16 09:01:09 -0500

    Copy views from Xamarin.Forms project

--- maui/Cinephile/Cinephile/App.xaml.cs
+++ maui/Cinephile/Cinephile/App.xaml.cs
@@ -1,4 +1,10 @@
-﻿namespace Cinephile
+﻿using Xamarin.Forms;
+using System.Net.Http;
+using FFImageLoading;
+using FFImageLoading.Config;
+using Fusillade;
+
+namespace Cinephile
 {
     public partial class App : Application
     {
@@ -6,7 +12,28 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            ImageService.Instance.Initialize(new Configuration
+            {
+                HttpClient = new HttpClient(new RateLimitedHttpMessageHandler(new HttpClientHandler(), Priority.Background))
+            });
+
+            new AppBootstrapper();
+            MainPage = AppBootstrapper.CreateMainPage();
+
+            // I hate to do this, but honestly dont know a better way to styke the navbar
+            ((NavigationPage)Current.MainPage).Style = Current.Resources["DefaultNavigationPageStyle"] as Style;
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
         }
     }
 }

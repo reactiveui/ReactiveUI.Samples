2f5df19103ca2dcdf557a52c165fc217ea6af809
Nathaniel Bond <nabond251@gmail.com>
2023-02-16 19:48:12 -0500

    Merge styles

--- maui/Cinephile/Cinephile/App.xaml.cs
+++ maui/Cinephile/Cinephile/App.xaml.cs
@@ -8,9 +8,6 @@
 
             new AppBootstrapper();
             MainPage = AppBootstrapper.CreateMainPage();
-
-            // I hate to do this, but honestly dont know a better way to styke the navbar
-            ((NavigationPage)Current.MainPage).Style = Current.Resources["DefaultNavigationPageStyle"] as Style;
         }
 
         protected override void OnStart()

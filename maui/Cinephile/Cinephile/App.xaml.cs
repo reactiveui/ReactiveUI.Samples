ec67521f751b1847bb51b37aa04e1158bae04572
nabond251 <nabond251@users.noreply.github.com>
2023-09-02 00:57:58 -0400

    Feature/maui cinephile (#698)
    * Add MAUI Cinephile project
    
    * Update project info
    
    * Copy styles from Xamarin.Forms project
    
    * Copy views from Xamarin.Forms project
    
    * Update namespaces
    
    * Remove FFImageLoading
    
    * Use Label instead of Span for gestures
    
    * Use event pattern directly
    
    * Fix style compile error
    
    * Update JSON annotations
    
    * Update image URLs
    
    See https://stackoverflow.com/a/50834600/1399272
    
    * Update Android colors
    
    * Merge styles
    
    * Update icons
    
    * Use fluent builder bootstrap
    
    ---------
    
    Co-authored-by: Chris Pulman <chris.pulman@yahoo.com>

---
+++ maui/Cinephile/Cinephile/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace Cinephile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = AppBootstrapper.CreateMainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}

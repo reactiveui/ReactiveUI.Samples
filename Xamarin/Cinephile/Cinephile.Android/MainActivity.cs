0bc8e72880000cb188e526877aaf63775c4f9ae2
Giusepe Casagrande <giusepe@gmail.com>
2023-11-17 14:34:33 -0300

    Created the xamarin project and fixed most of android issues, not all

--- Xamarin/Cinephile/Cinephile.Android/MainActivity.cs
+++ Xamarin/Cinephile/Cinephile.Android/MainActivity.cs
@@ -8,7 +8,7 @@ using Akavache;
 
 namespace Cinephile.Droid
 {
-    [Activity(Label = "Cinephile", Icon = "@mipmap/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    [Activity(Label = "Cinephile", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)

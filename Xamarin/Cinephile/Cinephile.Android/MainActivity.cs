72be97d156cf6d145a2f000a6c45ebeff041388a
Giusepe <giusepe@gmail.com>
2025-08-12 19:34:11 -0300

    Reorganize samples (#782)
    * Created the xamarin project and fixed most of android issues, not all
    
    * Fixed the last issues on Xamarin/Cinephile
    
    * create separated solutions for each sample and removed old/broken/useless samples
    
    ---------
    
    Co-authored-by: Chris Pulman <chris.pulman@yahoo.com>

--- Xamarin/Cinephile/Cinephile.Android/MainActivity.cs
+++ Xamarin/Cinephile/Cinephile.Android/MainActivity.cs
@@ -5,10 +5,11 @@ using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
 using Akavache;
+using FFImageLoading.Forms.Platform;
 
 namespace Cinephile.Droid
 {
-    [Activity(Label = "Cinephile", Icon = "@mipmap/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    [Activity(Label = "Cinephile", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -18,6 +19,7 @@ namespace Cinephile.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             BlobCache.ApplicationName = "Cinephile";
+            CachedImageRenderer.Init(true);
             LoadApplication(new App());
         }
 
